51b3d5d3882d1515803a3f4acf9efac50d5f0bdf
Irsyad <irsyad.muhd@gmail.com>
2017-03-01 23:20:09 +0800

    done setting up, react is yet to be implemented

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+
+const srcDir = resolve(__dirname, 'src')
+
+module.exports = {
+  entry: `${srcDir}/index.js`,
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'standard-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new DashboardPlugin()
+  ]
+}
